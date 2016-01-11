c1df46c784cbc395901efe7b3591d36b2f8562b0
Maël Regnery <mregnery@waycom.net>
2016-01-11 15:38:07 +0100

    Add example script

---
+++ example/barracuda.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib '../lib';
+use Barracuda::Api;
+
+my $bar = Barracuda::Api->new({
+	'domain'	=> 'mydomain.com',
+	'port'		=> 8000,
+	'password'	=> 'mypassword',
+});
+
+my $domain = $bar->listAllDomain();
+
+print $domain;
+
+1;
+__END__
